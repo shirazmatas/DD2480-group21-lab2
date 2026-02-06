ccacf791d95e8b1f083c5535eedbc67a66575f87
eryngia <barnabas.tanczos@gmail.com>
2026-02-07 00:45:38 +0100

    feat: implement clone and build project
    Implement part of build job to parse request, clone project to a new directory, then compile using Maven.
    
    Refs: #5

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,6 +1,8 @@
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.*;
+import org.json.*;
 import java.util.UUID;
 
 import org.eclipse.jetty.server.Server;
@@ -9,9 +11,10 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class ContinuousIntegrationServer extends AbstractHandler {
 
-    private static final String WEBHOOK_ENDPOINT = "/";
+    private static final String WEBHOOK_ENDPOINT = "/"; // TODO change endpoint on GitHub
+    private static final String BUILDS_DIRECTORY = "builds/";
 
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) {
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         System.out.println("Incoming request");
         System.out.println("    " + request.getMethod() + " " + target);
@@ -26,18 +29,75 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         } else if (!request.getMethod().equals("POST")) {
             response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
             System.out.println("Method not allowed");
-        } else if (!request.getHeader("X-GitHub-Event").equals("issue_comment")) { // TODO change to "push"
+        } else if (!"issue_comment".equals(request.getHeader("X-GitHub-Event"))) { // TODO change to "push"
             response.setStatus(HttpServletResponse.SC_OK);
             System.out.println("Event is not 'push'");
         } else {
             response.setStatus(HttpServletResponse.SC_OK);
-            new Thread(() -> runBuildJob(request)).start();
+            try {
+                JSONObject body = new JSONObject(new JSONTokener(request.getReader()));
+                new Thread(() -> runBuildJob(body)).start();
+            } catch (Exception e) {
+                System.out.println("Failed to parse request body");
+                e.printStackTrace();
+            }
         }
     }
 
-    private void runBuildJob(HttpServletRequest request) {
+    private void runBuildJob(JSONObject requestBody) {
         String buildId = UUID.randomUUID().toString();
-        System.out.println("Build " + buildId + ": job accepted");
+        String projectName = requestBody.getJSONObject("repository").getString("name");
+        String cloneUrl = requestBody.getJSONObject("repository").getString("clone_url");
+        String branch = requestBody.getString("ref").replace("refs/heads/", "");
+
+        System.out.println("Build " + buildId + ": job accepted for branch " + branch);
+
+        File buildDirectory = new File(BUILDS_DIRECTORY, "build-" + buildId);
+        File projectDirectory = new File(buildDirectory, projectName);
+
+        try {
+            // TODO update commit status on GitHub
+
+            if (!buildDirectory.mkdirs())
+                throw new RuntimeException("Failed to create build directory");
+
+            if (!runCommand(buildDirectory, "git", "clone", "--branch", branch, cloneUrl, projectDirectory.getAbsolutePath()))
+                throw new RuntimeException("Failed to clone repository");
+
+            if (!runCommand(projectDirectory, "mvnw.cmd", "clean", "compile")) {
+                System.out.println("Build " + buildId + ": compilation failed");
+                // TODO update commit status on GitHub
+                return;
+            }
+
+            System.out.println("Build " + buildId + ": compilation success");
+
+            // TODO run tests
+
+            // TODO cleanup
+
+            // TODO update commit status on GitHub
+
+        } catch (Exception e) {
+            System.out.println("Build " + buildId + ": unexpected error");
+            e.printStackTrace();
+        }
+    }
+
+    private boolean runCommand(File directory, String... command) throws IOException, InterruptedException {
+        Process process = new ProcessBuilder(command)
+                .directory(directory)
+                .redirectErrorStream(true)
+                .start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+
+        return process.waitFor() == 0;
     }
 
     public static void main(String[] args) throws Exception {
