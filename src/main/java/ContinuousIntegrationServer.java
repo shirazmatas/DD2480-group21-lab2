bb80d2b81f624e06a264b8111c530a461ef0e0e1
eryngia <barnabas.tanczos@gmail.com>
2026-02-06 19:12:03 +0100

    feat: validate HTTP requests
    Handle HTTP requests, checking for valid endpoint, method and event type. If valid, run thread for build job.
    
    Refs: #5

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,42 +1,46 @@
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 
-import java.io.IOException;
+import java.util.UUID;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-/** 
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
-public class ContinuousIntegrationServer extends AbstractHandler
-{
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response) 
-        throws IOException, ServletException
-    {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
+public class ContinuousIntegrationServer extends AbstractHandler {
+
+    private static final String WEBHOOK_ENDPOINT = "/";
+
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println("Incoming request");
+        System.out.println("    " + request.getMethod() + " " + target);
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(h -> System.out.println("    " + h + ": " + request.getHeader(h)));
+
         baseRequest.setHandled(true);
 
-        System.out.println(target);
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-
-        response.getWriter().println("CI job done");
+        if (!target.equals(WEBHOOK_ENDPOINT)) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            System.out.println("Endpoint not found");
+        } else if (!request.getMethod().equals("POST")) {
+            response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+            System.out.println("Method not allowed");
+        } else if (!request.getHeader("X-GitHub-Event").equals("issue_comment")) { // TODO change to "push"
+            response.setStatus(HttpServletResponse.SC_OK);
+            System.out.println("Event is not 'push'");
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+            new Thread(() -> runBuildJob(request)).start();
+        }
     }
 
-    // used to start the CI server in command line
-    public static void main(String[] args) throws Exception
-    {
+    private void runBuildJob(HttpServletRequest request) {
+        String buildId = UUID.randomUUID().toString();
+        System.out.println("Build " + buildId + ": job accepted");
+    }
+
+    public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         server.setHandler(new ContinuousIntegrationServer()); 
         server.start();
