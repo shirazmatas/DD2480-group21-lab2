cca6cf0d44baa9114eeb6412a1ed063d95b5518a
eryngia <barnabas.tanczos@gmail.com>
2026-02-07 01:16:28 +0100

    feat: improved readability of logs
    Print scope and type of server logs for increased readability.
    
    Refs: #5

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -14,31 +14,31 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     private static final String WEBHOOK_ENDPOINT = "/"; // TODO change endpoint on GitHub
     private static final String BUILDS_DIRECTORY = "builds/";
 
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) {
 
-        System.out.println("Incoming request");
-        System.out.println("    " + request.getMethod() + " " + target);
+        logInfo("Server","Incoming request");
+        logInfo("Server", "    " + request.getMethod() + " " + target);
         request.getHeaderNames().asIterator()
-                .forEachRemaining(h -> System.out.println("    " + h + ": " + request.getHeader(h)));
+                .forEachRemaining(h -> logInfo("Server","    " + h + ": " + request.getHeader(h)));
 
         baseRequest.setHandled(true);
 
         if (!target.equals(WEBHOOK_ENDPOINT)) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            System.out.println("Endpoint not found");
+            logInfo("Server", "Endpoint not found");
         } else if (!request.getMethod().equals("POST")) {
             response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-            System.out.println("Method not allowed");
+            logInfo("Server", "Method not allowed");
         } else if (!"issue_comment".equals(request.getHeader("X-GitHub-Event"))) { // TODO change to "push"
             response.setStatus(HttpServletResponse.SC_OK);
-            System.out.println("Event is not 'push'");
+            logInfo("Server", "Event is not 'push'");
         } else {
             response.setStatus(HttpServletResponse.SC_OK);
             try {
                 JSONObject body = new JSONObject(new JSONTokener(request.getReader()));
                 new Thread(() -> runBuildJob(body)).start();
             } catch (Exception e) {
-                System.out.println("Failed to parse request body");
+                logError("Server", "Failed to parse request body");
                 e.printStackTrace();
             }
         }
@@ -50,7 +50,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         String cloneUrl = requestBody.getJSONObject("repository").getString("clone_url");
         String branch = requestBody.getString("ref").replace("refs/heads/", "");
 
-        System.out.println("Build " + buildId + ": job accepted for branch " + branch);
+        logInfo("Build " + buildId, "Job accepted for branch " + branch);
 
         File buildDirectory = new File(BUILDS_DIRECTORY, "build-" + buildId);
         File projectDirectory = new File(buildDirectory, projectName);
@@ -61,16 +61,16 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             if (!buildDirectory.mkdirs())
                 throw new RuntimeException("Failed to create build directory");
 
-            if (!runCommand(buildDirectory, "git", "clone", "--branch", branch, cloneUrl, projectDirectory.getAbsolutePath()))
+            if (!runCommand(buildId, buildDirectory, "git", "clone", "--quiet", "--branch", branch, cloneUrl, projectDirectory.getAbsolutePath()))
                 throw new RuntimeException("Failed to clone repository");
 
-            if (!runCommand(projectDirectory, "mvnw.cmd", "clean", "compile")) {
-                System.out.println("Build " + buildId + ": compilation failed");
+            if (!runCommand(buildId, projectDirectory, "mvnw.cmd", "clean", "compile")) {
+                logInfo("Build " + buildId, "Compilation failed");
                 // TODO update commit status on GitHub
                 return;
             }
 
-            System.out.println("Build " + buildId + ": compilation success");
+            logInfo("Build " + buildId, "Compilation success");
 
             // TODO run tests
 
@@ -79,12 +79,12 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             // TODO update commit status on GitHub
 
         } catch (Exception e) {
-            System.out.println("Build " + buildId + ": unexpected error");
+            logError("Build " + buildId, "Unexpected error");
             e.printStackTrace();
         }
     }
 
-    private boolean runCommand(File directory, String... command) throws IOException, InterruptedException {
+    private boolean runCommand(String buildId, File directory, String... command) throws IOException, InterruptedException {
         Process process = new ProcessBuilder(command)
                 .directory(directory)
                 .redirectErrorStream(true)
@@ -93,11 +93,19 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                logInfo("Build " + buildId, line);
             }
         }
 
         return process.waitFor() == 0;
+    }
+
+    private void logInfo(String scope, String log) {
+        System.out.println("[INFO] [" + scope + "] " + log);
+    }
+
+    private void logError(String scope, String log) {
+        System.err.println("[ERROR] [" + scope + "] " + log);
     }
 
     public static void main(String[] args) throws Exception {
