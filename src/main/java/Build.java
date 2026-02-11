da1a95958fb30c5fca63e912aee3b3e2f1bbbde4
eryngia <barnabas.tanczos@gmail.com>
2026-02-11 17:57:36 +0100

    feat: save logs and metadata file
    Save console logs to a txt file and build metadata JSON in the build folder.
    
    Refs: #9

--- src/main/java/Build.java
+++ src/main/java/Build.java
@@ -1,5 +1,6 @@
 import org.json.JSONObject;
 import java.io.*;
+import java.time.Instant;
 import java.util.UUID;
 
 public class Build {
@@ -14,6 +15,10 @@ public class Build {
 
     private final File buildDirectory;
     private final File projectDirectory;
+    private final File metadataFile;
+
+    private final JSONObject metadata;
+    private final PrintWriter logWriter;
 
     public Build(JSONObject request) throws IOException {
         buildId = UUID.randomUUID().toString();
@@ -28,6 +33,15 @@ public class Build {
             throw new IOException("Failed to create build directory");
 
         projectDirectory = new File(buildDirectory, repository);
+        metadataFile = new File(buildDirectory, "metadata.json");
+        logWriter = new PrintWriter(new FileWriter(new File(buildDirectory, "build.log")));
+
+        metadata = new JSONObject();
+        metadata.put("buildId", buildId);
+        metadata.put("repository", repository);
+        metadata.put("branch", branch);
+        metadata.put("commit", commit);
+        metadata.put("startTime", Instant.now().toString());
     }
 
     public void run() {
@@ -53,8 +67,10 @@ public class Build {
             updateStatus("success");
 
         } catch (Exception e) {
-            logError("Unexpected error", e);
+            logError("Unexpected error during CI job", e);
             updateStatus("failure");
+        } finally {
+            logWriter.close();
         }
     }
 
@@ -74,16 +90,30 @@ public class Build {
     }
 
     private void updateStatus(String status) {
+        metadata.put("status", status);
+        metadata.put("endTime", Instant.now().toString());
+
+        try (FileWriter writer = new FileWriter(metadataFile)) {
+            writer.write(metadata.toString(4));
+        } catch (Exception e) {
+            logError("Unexpected error during metadata update", e);
+        }
+
         // TODO update commit status on GitHub
     }
 
     private void logInfo(String message) {
         System.out.println("[INFO] [Build " + buildId + "] " + message);
+        logWriter.println(message);
+        logWriter.flush();
     }
 
     private void logError(String message, Exception e) {
         System.err.println("[ERROR] [Build " + buildId + "] " + message);
         e.printStackTrace(System.err);
+        logWriter.println(message);
+        e.printStackTrace(logWriter);
+        logWriter.flush();
     }
 
 }
